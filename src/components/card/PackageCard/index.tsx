453f446daf0ac0f81bcb4f70b52f1afe93f411be
adibNo9 <adib.mokhtari@gmail.com>
2022-11-14 16:49:36 +0330

    Feat(branch name = VIT-37-card-component) implement packagecard component *build the PackageCard Component

--- src/components/card/PackageCard/index.tsx
+++ src/components/card/PackageCard/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
-const index = () => {
-  return <div>index</div>
+import Image from 'next/image'
+import { CardHeader, CardContent, CardMedia, Typography } from '@mui/material'
+import { PuzzleIcon } from '@assets/icon/PuzzleIcon'
+import styles from './styles.module.scss'
+
+export interface IPackageCard {
+  image?: string
+  title?: string
+  season?: string
+  leason?: string
 }
 
-export default index
+const PackageCard: React.FC<IPackageCard> = ({
+  image,
+  title,
+  season,
+  leason,
+}) => {
+  return (
+    <div className={styles['package-card']}>
+      <CardHeader avatar={<PuzzleIcon />} />
+      <CardMedia>
+        <Image src={image} alt="s" width="112" height="96" />
+      </CardMedia>
+      <CardContent>
+        <Typography variant="h6">{title}</Typography>
+        <Typography variant="body2">{season}</Typography>
+        <Typography variant="body2">{leason}</Typography>
+      </CardContent>
+    </div>
+  )
+}
+
+export default PackageCard
