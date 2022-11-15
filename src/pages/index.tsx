70ac7a94146fe811d7012e6d123775a9bdcdfb6c
adibNo9 <adib.mokhtari@gmail.com>
2022-11-15 12:31:49 +0330

    chore(TR-37): implement packagecard component *update index.tsx (deletion packageCard from view)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react'
-import PackageCard from '@components/card/PackageCard'
-import image from '@assets/images/package-image.png'
 
 const index = () => {
-  return (
-    <div>
-      <PackageCard
-        image={image.src}
-        title=" بسته زیست شناسی"
-        season="فصل اول: جهان هستی"
-        lesson="درس: چهارم "
-      />
-    </div>
-  )
+  return <div>index</div>
 }
 
 export default index
