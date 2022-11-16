ddcd0bee5096ca5f4b98e5b25a5064b6e6b5f9aa
adibNo9 <adib.mokhtari@gmail.com>
2022-11-16 17:00:20 +0330

    chore(TR-34): implement TextField component *clear root page after finish work of textfield

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,7 @@ import React, { useState } from 'react'
 import TextField from '@components/form/components/MainTextField'
 
 const index = () => {
-  const [value, setValue] = useState<string>('')
-
-  const error = false
-
-  return (
-    <div>
-      <TextField
-        label="کد ملی"
-        id="national-code"
-        type="text"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        onBlur={() => console.log('blur')}
-        onFocus={() => console.log('focus')}
-        validationError={error && 'چیزی را اشتباه وارد کردیدو دوباره سعی کنید'}
-        error={error}
-      />
-    </div>
-  )
+  return <div>root page</div>
 }
 
 export default index
