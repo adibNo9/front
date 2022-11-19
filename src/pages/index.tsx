84ac276f812e3edf15703c7bbf03810ae82c0fc9
adibNo9 <adib.mokhtari@gmail.com>
2022-11-19 16:55:30 +0330

    Chore(TR-70): implement Otp component *update root for view otp timer. *set resendCode function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,19 @@ import MainOtp from '@components/form/components/MainOtp'
 const index = () => {
   const [otp, setOtp] = useState('')
   const onChange = (value: string) => setOtp(value)
+
+  const resendCodeClickHandler = () => {
+    //desired function to be performed on clicking resend button
+    console.log('resend code')
+  }
   return (
     <div>
-      <MainOtp value={otp} valueLength={6} onChange={onChange} />
+      <MainOtp
+        value={otp}
+        valueLength={5}
+        onChange={onChange}
+        resendOtpCode={resendCodeClickHandler}
+      />
     </div>
   )
 }
