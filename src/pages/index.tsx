53822d4ca85751c603047b2f80249f5df94eb169
alimoqimi97 <alimoqimi97@gmail.com>
2022-11-19 14:48:29 +0330

    Chore(TR-35): remove unnecessary code
    -remove unnecessary code from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import MuiTextField from '@components/form/components/MuiTextField'
-import FormSelectInput from '@components/form/components/SelectInput/FormSelectInput'
-import { Box } from '@mui/material'
 import React, { useState } from 'react'
-
+import MainSelectInput from '@components/form/components/MainSelectInput'
+import { Box } from '@mui/material'
 import { SelectChangeEvent } from '@mui/material/Select'
 
 const names: string[] = [
@@ -13,57 +11,7 @@ const names: string[] = [
 ]
 
 const index = () => {
-  const [selected, setSelected] = useState<string>('')
-
-  const handleChange = (event: SelectChangeEvent<string>): void => {
-    const {
-      target: { value },
-    } = event
-    setSelected(value)
-  }
-
-  return (
-    <Box
-      sx={{
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        margin: 'auto',
-        border: '1px solid #666',
-        marginTop: 20,
-        marginBottom: 20,
-        flexDirection: 'column',
-      }}
-    >
-      <FormSelectInput
-        stateValue={selected}
-        onChange={handleChange}
-        title='نام'
-        items={names}
-      />
-      <MuiTextField
-        label="کد ملی"
-        fullWidth
-        id="nationalCode"
-        type="text"
-        helperText="کد ملی خود را وارد کنید"
-      />
-      <MuiTextField
-        label="کد ملی"
-        fullWidth
-        id="nationalCode"
-        type="text"
-        helperText="کد ملی خود را وارد کنید"
-      />
-      <MuiTextField
-        label="کد ملی"
-        fullWidth
-        id="nationalCode"
-        type="text"
-        helperText="کد ملی خود را وارد کنید"
-      />
-    </Box>
-  )
+ return (<div>root </div>)
 }
 
 export default index
