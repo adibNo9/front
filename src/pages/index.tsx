6a3c25451f6b0ddbeb13162874edd0b3ee64e80b
alimoqimi97 <alimoqimi97@gmail.com>
2022-11-19 15:00:57 +0330

    Chore(TR-35): remove unneccessary code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,4 @@
-import React, { useState } from 'react'
-import MainSelectInput from '@components/form/components/MainSelectInput'
-import { Box } from '@mui/material'
-import { SelectChangeEvent } from '@mui/material/Select'
-
-const names: string[] = [
-  'ali',
-  'mohamad',
-  'hassan',
-  'hossein',
-]
+import React from 'react'
 
 const index = () => {
  return (<div>root </div>)
