00c2d872b872612ee58de4adcda8755f729ae1fe
alimoqimi97 <alimoqimi97@gmail.com>
2022-11-20 16:12:54 +0330

    Chore(TR-71): add register levels images
    -add register images.

--- src/components/layout/form/index.tsx
+++ src/components/layout/form/index.tsx
@@ -1,27 +1,32 @@
 import React from 'react'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import FormContainer from './components/FormContainer/FormTabs';
 import styles from './FormLayout.module.scss';
-
-export interface IPic {
-    src: string,
-    width?: number,
-    height?: number,
-    className?: string,
-}
+import login from '@assets/images/login.png'
+import changePassword from '@assets/images/changePassword.png'
+import otp from '@assets/images/otp.png'
+import signup from '@assets/images/signup.png'
 
 export interface IFormLayout {
     children?: React.ReactNode,
-    imageProps: IPic,
 }
 
-const FormLayout: React.FC<IFormLayout> = ({ imageProps }) => {
+const FormLayout: React.FC<IFormLayout> = () => {
+  // to do: add context and read step from it in form levels.
+  const stemImages: StaticImageData[] = [
+    login,
+    changePassword,
+    otp,
+    signup
+  ];
+
+  const step: number = 0;
+
   return (
         <section dir='ltr' className={styles['form-layout-container']}>
             <div className={styles['form-image-wrapper']}>
                 <Image
-                    {...imageProps}
-
+                    src={stemImages[step]}
                   />
             </div>
             <main>
