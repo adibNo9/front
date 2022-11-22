bc68f94686210b9cca7fb6351d0164adda30896e
alimoqimi97 <alimoqimi97@gmail.com>
2022-11-22 14:15:26 +0330

    Chore(TR-89): Implemenent FormReferralCode
    -add props.
    -add label.

--- src/components/form/components/FormReferralCode/index.tsx
+++ src/components/form/components/FormReferralCode/index.tsx
@@ -1,13 +1,39 @@
-
-
+import { ChangeEvent, FC } from 'react'
+import MainInput from '../MainTextField'
+import styles from './styles.module.scss'
 
 interface IFormReferralCode {
-    text: string,
-    onChange?: () => void,
+    id: string,
+    label: string,
+    value?: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    onBlur: () => void
+    onFocus: () => void
 }
 
 const FormReferralCode: FC<IFormReferralCode> = ({
-
+  id,
+  label,
+  onChange,
+  onBlur,
+  onFocus,
+  value,
+  ...otherProps
 }) => {
-
+  return (
+        <div className={styles['form-referral-code']}>
+            <label>{label}</label>
+            <MainInput
+                id={id}
+                type='number'
+                value={value || ''}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onChange={onChange}
+                {...otherProps}
+            />
+        </div>
+  )
 }
+
+export default FormReferralCode
