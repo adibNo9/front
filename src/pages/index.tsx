fffe7e5334626c5fbbb1520a4c59d203331792cf
adibNo9 <adib.mokhtari@gmail.com>
2022-11-26 15:32:46 +0330

    Chore(TR-38): implement Header component *clear root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import StudentLayout from '@components/layout/student/StudentLayout'
 import React from 'react'
 
 const index = () => {
-  return (
-    <StudentLayout>
-      <div> </div>
-    </StudentLayout>
-  )
+  return <div> </div>
 }
 
 export default index
